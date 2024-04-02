e098faebfe7678bd1e71f26819559cce8899bf49
Ashley Green <ashleygreen1395@gmail.com>
2024-04-02 17:21:10 -0400

    first commit

---
+++ src/components/Pirate.js
@@ -0,0 +1,30 @@
+import Button from "./Button";
+import "../assets/css/Pirate.css";
+import avatar from "../assets/img/avatar.png";
+
+function Pirate({
+  pirate: { name, year, weapon, vessel, desc, id },
+  tagline,
+  removePirate,
+}) {
+  return (
+    <section>
+      <summary>
+        <img src={avatar} alt="pirate" />
+        <h3>{name}</h3>
+        <ul>
+          <li>Died: {year}</li>
+          <li>Weapon: {weapon}</li>
+          <li>Vessel: {vessel}</li>
+        </ul>
+      </summary>
+      <article>
+        <h2>{tagline}</h2>
+        <p>{desc}</p>
+        <Button onClick={() => removePirate(id)} text="Remove Pirate" />
+      </article>
+    </section>
+  );
+}
+
+export default Pirate;
